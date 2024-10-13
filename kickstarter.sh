e2ca23e29bad575d65c708be2463d3f7fcef4c6e
Mateusz Sieprawski <me@mattb.it>
2024-10-13 12:55:38 +0200

    Removed beta images

--- kickstarter.sh
+++ kickstarter.sh
@@ -21,7 +21,7 @@ sed -i '' "s/{appname}/$1/g" docker-compose.local.yml
 
 # Download Laravel
 path=$(pwd)
-docker run -it -v $path:/var/www/html serversideup/php:beta-8.3-cli composer create-project laravel/laravel laravel
+docker run -it -v $path:/var/www/html serversideup/php:8.3-cli composer create-project laravel/laravel laravel
 
 # Copy all files and folders (even hidden) from laravel to the root of the project and remove laravel folder
 cp -a laravel/. .
@@ -34,6 +34,6 @@ cp .env.example .env
 docker-compose -f docker-compose.local.yml build
 
 # Configure application key by running php artisan key:generate on the {appname}_webserver container
-docker run -it -v $path:/var/www/html serversideup/php:beta-8.3-cli php artisan key:generate
+docker run -it -v $path:/var/www/html serversideup/php:8.3-cli php artisan key:generate
 
 echo "✅ Project $1 has been kickstarted successfully! Build something awesome! 🚀"
